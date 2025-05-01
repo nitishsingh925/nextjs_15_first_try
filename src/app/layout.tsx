1f51bf1cf25c4489e6219be46acc0b55aebbf620
Nitish Singh <76509326+nitishsingh925@users.noreply.github.com>
2025-05-01 22:03:50 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
